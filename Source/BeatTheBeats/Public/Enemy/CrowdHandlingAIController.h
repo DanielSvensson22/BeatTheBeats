f087a9df4d9c442028103db19971349f181f371a
DanielSvensson22 <111700515+DanielSvensson22@users.noreply.github.com>
2024-10-10 08:18:44 +0200

    Reapply "See below"
    This reverts commit f5cc0f9ca63e7a2676d3e922f93aafa42e0d791b.

---
+++ Source/BeatTheBeats/Public/Enemy/CrowdHandlingAIController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CrowdHandlingAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEATTHEBEATS_API ACrowdHandlingAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	ACrowdHandlingAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
